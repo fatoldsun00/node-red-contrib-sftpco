f886c8e713360d4da23fed29543763e003d57ee5
unknown <fatoldsun00@gmail.com>
2020-04-23 16:27:40 +0200

    bug on output get put delete

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# node-red-contrib-sftp
-<p>An SFTP client for node-red, a wrapper for <a href="https://github.com/jyu213/ssh2-sftp-client">ssh2-sftp-client</a></p>
+# node-red-contrib-sftpco
+<p>An SFTP client for node-red, wrapper for <a href="https://github.com/jyu213/ssh2-sftp-client">ssh2-sftp-client</a></p>
+
+[![BuyMeCoffee][buymecoffee-shield]][buymecoffee-link]
+
+## Why anoher node for SFTP connection ?
+
+
+
+The other watcher (wfwatcher) check recursively and attach filename to payload... Most of nodes who work with file check msg.filename.
+
+So this plugin attach to msg :
+ - file : the name of the file, with extension
+ - filedir : Directory of file
+ - filename: the complete path to the file
+ - payload: the complete path to the file
+
+On configuration side we can : 
+
+ - Select the folder to watch (of course ....)
+ - Type event : create, update, delete
+ - Check for recursively or not
+ - ignore file who are in directory are start (or not)
+ - ignore files on regex based, only String !, not type the / for regextype (I.E. ^test\.(txt|xls) not /^test\.(txt|xls)/)
+
+### Prerequisites
+
+Have Node-RED installed and working, if you need to
+install Node-RED see [here](https://nodered.org/docs/getting-started/installation).
+
+- [Node.js](https://nodejs.org) v10.0 or newer
+- [Node-RED](https://nodered.org/) v1.0 or newer
+
+### Installation
+ 
+Install via Node-RED Manage Palette
+
+```
+node-red-contrib-watchdirectory
+```
+
+Install via npm
+
+```shell
+$ cd ~/.node-red
+$ npm install node-red-contrib-watchdirectory
+# then restart node-red
+```
+
+## Contribute
+
+PR are welcome
+
+[release-link]: https://github.com/fatoldsun00/node-red-contrib-watchdirectory
+[release-shield]: https://img.shields.io/github/v/release/zachowj/node-red-contrib-home-assistant-websocket?style=for-the-badge
+[buymecoffee-link]: https://www.buymeacoffee.com/fatoldsun00
+[buymecoffee-shield]: https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png
---
+++ node-red-sftpco.js
@@ -0,0 +1,161 @@
+
+module.exports = function(RED) {
+	//var SFTPCredentialsNode, SFTPNode;
+	const Client = require('ssh2-sftp-client');
+	const path = require('path')
+	const fs = require('fs')
+
+	function SFTPCredentialsNode(config) {
+		RED.nodes.createNode(this,config)
+		this.host = config.host
+		this.port = config.port
+		this.username = config.username
+		this.password = config.password
+	}
+
+	function SFTPNode(config) {
+		RED.nodes.createNode(this, config)
+		let key,  value, node = this
+		
+		for (key in config) {
+			value = config[key];
+			node[key] = value;
+		}
+
+		node.server = RED.nodes.getNode(config.server);
+		
+		this.listenInput()
+	}
+
+	SFTPNode.prototype.listenInput = function (){
+		let node = this
+		const sftp =  new Client();
+		
+		node.on('input', async function(msg) {
+			try {
+				node.status({
+					fill: "grey",
+					shape: "dot",
+					text: "connecting"
+				});
+	
+				await sftp.connect({ 
+					host: node.server.host,
+					port: node.server.port,
+					username: node.server.username,
+					password: node.server.password});
+
+
+				node.status({
+					fill: "yellow",
+					shape: "ring",
+					text: "connected"
+				});
+	
+				let remoteFilePath, localFilePath, payload
+				switch(node.method) {
+					case "list":
+						remoteFilePath = RED.util.evaluateNodeProperty(node.remoteFilePath,node.remoteFilePathType,node,msg)
+						if (!remoteFilePath) throw new Error('Remote path undefined')
+						payload = await sftp.list(remoteFilePath)
+						msg.payload = payload.map((r)=>{	
+							return remoteFilePath+"/"+r.name
+						})
+						node.send(msg)
+					break;
+							
+					case "get":
+						remoteFilePath = RED.util.evaluateNodeProperty(node.remoteFilePath,node.remoteFilePathType,node,msg)
+						localFilePath = RED.util.evaluateNodeProperty(node.localFilePath,node.localFilePathType,node,msg)
+
+						if (!remoteFilePath) throw new Error('Remote path undefined')
+						if (!localFilePath) throw new Error('Local path undefined')
+
+						let localFilePathIsDir = fs.statSync(localFilePath).isDirectory()
+
+						if (Array.isArray(remoteFilePath)){
+							payload=[]
+							for(remotePath of remoteFilePath){
+								let file = remotePath.substring(remotePath.lastIndexOf('/')+1)
+								let localFile = localFilePath
+								if (localFilePathIsDir) localFile = path.join(localFilePath,file)
+								localFile=path.normalize(localFile)
+								payload.push(await sftp.get(remotePath,localFile))
+							}
+						} else {
+							let file = remoteFilePath.substring(remoteFilePath.lastIndexOf('/')+1)	
+							let localFile = localFilePath
+							if (localFilePathIsDir) localFile = path.join(localFilePath,file)
+							await sftp.get(remoteFilePath,localFile)
+						}
+
+						msg.payload = payload
+						node.send([msg,null])
+					break;
+
+					case "put":
+						remoteFilePath = RED.util.evaluateNodeProperty(node.remoteFilePath,node.remoteFilePathType,node,msg)
+						localFilePath = RED.util.evaluateNodeProperty(node.localFilePath,node.localFilePathType,node,msg)
+
+						if (!remoteFilePath) throw new Error('Remote path undefined')
+						if (!localFilePath) throw new Error('Local path  undefined')
+						
+						if (Array.isArray(localFilePath)){
+							payload=[]
+								for(localPath of localFilePath){
+								localPath=path.normalize(localPath)
+								let file = path.basename(localPath)
+								await sftp.put(localPath,path.posix.join(remoteFilePath,file))
+								payload.push(localPath)
+							}
+						} else {
+							localFilePath=path.normalize(localFilePath)
+							let file = path.basename(localFilePath)
+							await sftp.put(localFilePath,path.posix.join(remoteFilePath,file))
+							payload = localFilePath
+						}
+
+						msg.payload = payload
+						node.send([msg,null])
+					break;	
+					case "delete":
+						remoteFilePath = RED.util.evaluateNodeProperty(node.remoteFilePath,node.remoteFilePathType,node,msg)
+						if (!remoteFilePath) throw new Error('Remote path undefined')
+						
+						if (Array.isArray(remoteFilePath)){
+							payload=[]
+							for(remotePath of remoteFilePath){
+								await sftp.delete(remotePath)
+								payload.push(remotePath)
+							}
+						} else {
+							await sftp.delete(remoteFilePath)
+							payload = remoteFilePath
+						}
+						msg.payload = payload
+						node.send([msg,null])
+					break;	
+				}
+
+				node.status({
+					fill: "green",
+					shape: "dot",
+					text: node.method+" done !"
+				});
+			} catch (err) {
+				console.log(err);
+				node.status({
+					fill: "red",
+					shape: "dot",
+					text: "Error :"+ err.message
+				});
+				node.send([null,{err,inputMsg: msg}])
+			} finally {
+				sftp.end()
+			}
+		})
+	}
+
+	RED.nodes.registerType("SFTP-credentials", SFTPCredentialsNode);
+	RED.nodes.registerType("SFTP-main", SFTPNode);
+};
